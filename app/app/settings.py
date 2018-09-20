6b220c5d979373adde7c913bacc26b85586291f7
wanglong <18767159636@qq.com>
2018-09-20 14:55:45 +0800

    修改设置

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'interface'
+    'interface',
+    'weixin'
 )
 
 MIDDLEWARE_CLASSES = (
